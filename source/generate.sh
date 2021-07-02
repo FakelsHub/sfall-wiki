b112f5b9e32b21bd64a6cd2dc6755e08871117ed
Mr.Stalin <Fakels@yandex.ru>
2021-07-03 00:15:05 +0300

    Fixed.

--- source/generate.sh
+++ source/generate.sh
@@ -7,8 +7,8 @@ yml_dir="../docs"
 pages_dir="pages"
 
 cd "$site_dir"
-echo "sfall.bgforge.net" > CNAME
 mkdir -p "$pages_dir"
+cp ../docs/*.md .
 ./yaml_to_md.py "$yml_dir/functions.yml" "$yml_dir/hooks.yml" "$pages_dir"
 
 bundle install
